4a8899aee96797a6949dc015d45e9f575fa33992
Melih <66446048+Melo3803@users.noreply.github.com>
2025-08-29 17:38:44 +0300

    Update app.h

--- kernel/src/apps/app.h
+++ kernel/src/apps/app.h
@@ -1,10 +1,10 @@
 #pragma once
+#include "../Framebuffer.h"
 #include "../userinput/mouse.h"
 
-class Application{
-    public:
-    Application();
-    ~Application();
-
-    virtual void Run();
+class Application {
+public:
+    virtual ~Application() = default;
+    virtual void OnStart() {}; // Başlangıçta bir kez çalışır
+    virtual void OnUpdate() {}; // Sürekli çalışır
 };
