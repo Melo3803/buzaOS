faaed0236d0ec68d2d5c1ee0c3eb1539fc67ad21
Melih <66446048+Melo3803@users.noreply.github.com>
2025-08-29 17:40:26 +0300

    Optimizing the code and bug fix

--- kernel/src/apps/paint.cpp
+++ kernel/src/apps/paint.cpp
@@ -1,40 +1,27 @@
 #include "paint.h"
 #include "../BasicRenderer.h"
-#include "../apps/appManager.h"
+#include "appManager.h"
 
-extern bool LeftMouseDown; // Fare durumunu dışarıdan alacağız
+extern bool LeftMouseDown;
 extern bool RightMouseDown;
 
-PaintApp::PaintApp(){
-
-    
-}
-
-PaintApp::~PaintApp(){
-
-}
-
-void PaintApp::Run(){
-    if (LeftMouseDown){
-        GlobalRenderer->PutChar('a', MousePosition.X, MousePosition.Y);
-        
-    }
-    GlobalRenderer->ClearColour = 0x00000000; //arkaplan rengi
+void PaintApp::OnStart(){
+    GlobalRenderer->ClearColour = 0xffffffff; // Tuval rengini beyaza ayarla
+    GlobalRenderer->Clear(); // Tuvali temizle
     GlobalRenderer->XCenter();
-    
+    GlobalRenderer->Colour = 0xff000000;
     GlobalRenderer->Print("Paint - Cikmak icin sag tikla");
-    
+}
+
+void PaintApp::OnUpdate(){
+    // Sağ tuş ile masaüstüne dön
     if (RightMouseDown){
-        
         GlobalAppManager->SwitchTo(GlobalAppManager->Desktop);
-        GlobalRenderer->Clear();
-        }
-    
-    
-    /* if(GlobalAppManager->CurrentApp == GlobalAppManager->Paint){
-                    GlobalAppManager->SwitchTo(GlobalAppManager->Desktop);
-                }
-                else if(GlobalAppManager->CurrentApp == GlobalAppManager->Buza){
-                    GlobalAppManager->SwitchTo(GlobalAppManager->Desktop);
-                }*/
+    }
+
+    // Sol tuş basılıyken çizim yap
+    if (LeftMouseDown){
+        GlobalRenderer->Colour = 0xff000000;
+        GlobalRenderer->PutChar('a',MousePosition.X, MousePosition.Y); // Siyah renkle çiz
+    }
 }
