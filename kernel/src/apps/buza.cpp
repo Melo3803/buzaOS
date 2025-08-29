913970e45449622dafe4e687ae4c1e30c51616ff
Melih <66446048+Melo3803@users.noreply.github.com>
2025-08-29 17:39:32 +0300

    Bug fix

--- kernel/src/apps/buza.cpp
+++ kernel/src/apps/buza.cpp
@@ -1,47 +1,44 @@
 #include "buza.h"
 #include "../BasicRenderer.h"
-#include "../apps/appManager.h"
+#include "appManager.h"
 
-extern bool LeftMouseDown; // Fare durumunu dışarıdan alacağız
+
 extern bool RightMouseDown;
 
-BuzaApp::BuzaApp(){
+const int DUMMY_WIDTH = 16;
+const int DUMMY_HEIGHT = 16;
+unsigned char dummy_rgba[DUMMY_WIDTH * DUMMY_HEIGHT * 4];
 
-}
-
-BuzaApp::~BuzaApp(){
-    
-}
-
-
-void BuzaApp::Run(){
-
-        // arkaplan rengi
-        GlobalRenderer->ClearColour = 0xff00ffff;
-        GlobalRenderer->XCenter();
-        GlobalRenderer->Print("BuzaApp - Cikmak icin sag tikla");
-            
-
-        GlobalRenderer->CursorPosition = {50, 50};
-        GlobalRenderer->Colour = 0xffff00ff;
-        GlobalRenderer->Print("Buza App");
-    
-
-    
-
-    
-    if (RightMouseDown){
-        
-        GlobalAppManager->SwitchTo(GlobalAppManager->Desktop);
-        GlobalRenderer->Clear();
+void CreateDummyImage() {
+    for (int y = 0; y < DUMMY_HEIGHT; y++) {
+        for (int x = 0; x < DUMMY_WIDTH; x++) {
+            int idx = (y * DUMMY_WIDTH + x) * 4;
+            dummy_rgba[idx + 0] = 255; // R
+            dummy_rgba[idx + 1] = 0;   // G
+            dummy_rgba[idx + 2] = 0;   // B
+            dummy_rgba[idx + 3] = 255; // A
         }
+    }
+}
 
-    
-    
-    /* if(GlobalAppManager->CurrentApp == GlobalAppManager->Paint){
-                    GlobalAppManager->SwitchTo(GlobalAppManager->Desktop);
-                }
-                else if(GlobalAppManager->CurrentApp == GlobalAppManager->Buza){
-                    GlobalAppManager->SwitchTo(GlobalAppManager->Desktop);
-                }*/
+void BuzaApp::OnStart(){
+    GlobalRenderer->ClearColour = 0xff00ffff;
+    GlobalRenderer->Clear();
+    GlobalRenderer->XCenter();
+    GlobalRenderer->Colour = 0xffff00ff;
+    GlobalRenderer->Print("BuzaApp - Cikmak icin sag tikla");
+    GlobalRenderer->CursorPosition = {50, 50};
+    GlobalRenderer->Print("BUZA:");
+
+    CreateDummyImage(); // Resmi hazırla
+
+    int startX = (GlobalRenderer->TargetFramebuffer->Width - DUMMY_WIDTH) / 2;
+    int startY = (GlobalRenderer->TargetFramebuffer->Height - DUMMY_HEIGHT) / 2;
+    GlobalRenderer->DrawImageRGBA(dummy_rgba, DUMMY_WIDTH, DUMMY_HEIGHT, startX, startY);
+}
+
+void BuzaApp::OnUpdate(){
+    if (RightMouseDown){
+        GlobalAppManager->SwitchTo(GlobalAppManager->Desktop);
+    }
 }
